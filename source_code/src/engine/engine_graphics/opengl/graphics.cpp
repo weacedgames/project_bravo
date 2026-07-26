c7c46ab3af045f708cd4d0d961623fec235465b8
itzweaced <lorenzoasrich@gmail.com>
2026-07-25 17:17:07 -0700

    Edited

--- source_code/src/engine/engine_graphics/opengl/graphics.cpp
+++ source_code/src/engine/engine_graphics/opengl/graphics.cpp
@@ -7,7 +7,7 @@
 // SUMMARY METADATA:                                                      //
 // -> Company: WeAcEd GaMeS                                               //
 // -> Author First Name: The                                              //
-// -> Author First Name: Programmer                                       //
+// -> Author Last Name: Programmer                                        //
 // -> Version: version.alpha.1.0                                          //
 // -> File: graphics.cpp                                                  //
 // -> Date Created: 07-25-2026                                            //
